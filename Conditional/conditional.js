3b90691a4565df1714a6479ed826407a92630496
anya-uwu <anyadev@proton.me>
2024-03-11 12:19:23 -0400

    finished conditionals

---
+++ Conditional/conditional.js
@@ -0,0 +1,15 @@
+
+let random = Math.random
+
+if (1 + 1 === 2) {
+    console.log("math works")
+}
+
+const dayOfWeek = 'Monday';
+
+if (dayOfWeek === 'Monday') {
+    console.log('garfield hates mondays');
+} else if (dayOfWeek === 'Saturday') {
+    console.log('lets eat lasagna!');
+};
+
