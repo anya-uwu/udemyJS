640a80b435e2e9928b0a01467cadaa4e0e6466bd
anya-uwu <anyadev@proton.me>
2024-03-18 16:31:46 -0400

    finished loops, started functions

---
+++ Functions/app.js
@@ -0,0 +1,11 @@
+function singSong() {
+    console.log('lalalalalala')
+}
+
+singSong();
+
+function greet(person) {
+    console.log(`Hi ${person}!`)
+}
+
+greet('Shelby');
--- ToDoApp/app.js
+++ ToDoApp/app.js
@@ -13,9 +13,14 @@ while (input !== 'quit' && input !== 'q') {
         todos.push(newTodo);
         console.log(`${newTodo} added to the list!`)
     } else if (input === 'delete') {
-        const index = prompt('Enter an index to delete:');
-        const deleted = todos.splice(index, 1);
-        console.log(`Ok, deleted ${deleted}`);
+        const index = parseInt(prompt('Enter an index to delete:'));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok, deleted ${deleted}`);
+        } else {
+            console.log('Not a valid index.')
+        }
+        
     }
     input = prompt('what would you like to do?')
 }
