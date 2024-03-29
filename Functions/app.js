723da379284ebdf1e94def416bc2400a64d457b8
anya-uwu <anyadev@proton.me>
2024-03-28 21:02:12 -0400

    worked on functions

--- Functions/app.js
+++ Functions/app.js
@@ -4,8 +4,18 @@ function singSong() {
 
 singSong();
 
-function greet(person) {
-    console.log(`Hi ${person}!`)
+function greet(person, age) {
+    console.log(`Hi ${person}! You are ${age} years old`)
 }
 
-greet('Shelby');
+greet('Shelby', '18');
+
+function repeat(str, numTimes) {
+    let result = '';
+    for(let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result)
+}
+
+repeat('hi', 2)
